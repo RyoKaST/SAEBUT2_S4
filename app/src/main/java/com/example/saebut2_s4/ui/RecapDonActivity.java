be5e7414ee9a25ac3f6a1670d1fa40b3c8f1a32b
abedhab <121730209+abedhab@users.noreply.github.com>
2025-03-25 04:46:15 +0100

    page EffectuerDonActivity et page RecapDonActivity

---
+++ app/src/main/java/com/example/saebut2_s4/ui/RecapDonActivity.java
@@ -0,0 +1,26 @@
+package com.example.saebut2_s4.ui;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.saebut2_s4.R;
+
+public class RecapDonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_recap_don);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
