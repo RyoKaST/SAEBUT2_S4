577d31f336736f40d88ff8ed3d73f2a74931ba51
abedhab <121730209+abedhab@users.noreply.github.com>
2025-03-25 03:37:32 +0100

    rename

---
+++ app/src/main/java/com/example/saebut2_s4/ui/ConnexionActivity.java
@@ -0,0 +1,40 @@
+package com.example.saebut2_s4.ui;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.saebut2_s4.R;
+
+public class ConnexionActivity extends AppCompatActivity {
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_connexion);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+    public void onSkipClicked(View view) {
+        // Créer une intention pour passer à l'activité AccueilActivity
+        Intent intent = new Intent(ConnexionActivity.this, AccueilActivity.class);
+
+        // Démarre l'activité AccueilActivity
+        startActivity(intent);
+
+        // Optionnel : Finir l'activité LoginActivity pour ne pas revenir en arrière
+        finish();
+    }
+}
