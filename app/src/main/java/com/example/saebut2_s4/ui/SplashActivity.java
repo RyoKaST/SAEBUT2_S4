b6daab587feae7844b045d81780e34218f33911b
abedhab <121730209+abedhab@users.noreply.github.com>
2025-03-25 03:10:25 +0100

    page SplashActivity + Connection

--- app/src/main/java/com/example/saebut2_s4/ui/SplashActivity.java
+++ app/src/main/java/com/example/saebut2_s4/ui/SplashActivity.java
@@ -2,25 +2,55 @@ package com.example.saebut2_s4.ui;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+
+import android.content.Intent;
+import android.os.Handler;
+import android.widget.ProgressBar;
 
 import com.example.saebut2_s4.R;
+import com.example.saebut2_s4.ui.AcceuilActivity;
+import com.example.saebut2_s4.ui.ConnectionActivity;
 
 public class SplashActivity extends AppCompatActivity {
+
+    private ProgressBar progressBarHorizontal;
+    private int progressStatus = 0;
+    private Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        // Trouver les ProgressBars
+        progressBarHorizontal = findViewById(R.id.progress_horizontal);
+
+        // Thread pour animer la barre de chargement
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (progressStatus < 100) {
+                    progressStatus += 1;
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            progressBarHorizontal.setProgress(progressStatus);
+                        }
+                    });
+                    try {
+                        Thread.sleep(50); // Vitesse de remplissage
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                // Une fois la barre remplie, on passe à MainActivity
+                Intent intent = new Intent(SplashActivity.this, ConnectionActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }).start();
     }
 }
