d065ec760770438068a79a3a75ae8cc4f8f4f90a
Lukas <lukasstavropoulos94@gmail.com>
2025-03-30 01:43:50 +0100

    LES FRAGMENTS PTDR (plus facile à faire que la navbar)

---
+++ app/src/main/java/com/example/saebut2_s4/ui/fragment/FavoritesFragment.java
@@ -0,0 +1,10 @@
+package com.example.saebut2_s4.ui.fragment;
+import androidx.fragment.app.Fragment;
+
+import com.example.saebut2_s4.R;
+
+public class FavoritesFragment extends Fragment {
+    public FavoritesFragment() {
+        super(R.layout.fragment_favorites);
+    }
+}
